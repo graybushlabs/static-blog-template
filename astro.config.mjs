240a52cdf853b72df9d5f0525844336306588dff
John Stillwell <stillwell.john@gmail.com>
2024-08-20 16:01:37 -0400

    remove graybush labs specific stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://graybush.io',
+  site: 'https://blog.example.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
